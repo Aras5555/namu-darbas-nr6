b116238948b01e8340976b17275d5d90e821439e
Arunas <akakstys@gmail.com>
2019-05-28 11:05:22 +0300

    13

---
+++ 13darbas/scripts.js
@@ -0,0 +1,28 @@
+function f() {document.getElementById("S").innerHTML= S;
+             document.getElementById("P").innerHTML= P;
+             document.getElementById("d").innerHTML= d;}
+
+
+
+
+ var x=5;
+ var y=7;
+
+
+  var S= x * y;
+  var P = 2* ( x+ y);
+  var d = Math.sqrt((x**2)+(y**2));
+
+ console.log(S);
+ console.log(P);
+ console.log(d);
+
+
+
+
+
+
+
+
+
+
